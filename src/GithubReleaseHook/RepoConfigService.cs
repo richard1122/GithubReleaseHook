6a256663c1855ab3e5e5bb2542831e4cc16cdbe6
Richard <richard9372@gmail.com>
2016-08-25 15:28:26 +0800

    add docker

--- src/GithubReleaseHook/RepoConfigService.cs
+++ src/GithubReleaseHook/RepoConfigService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using GithubReleaseHook.models;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
